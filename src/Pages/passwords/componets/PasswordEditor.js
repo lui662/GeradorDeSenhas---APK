d5496122a2f0965f1d7482250f2b08bda2926c32
lui662 <lokateu.fs@gmail.com>
2025-04-18 19:34:22 -0300

    adicionado sistema de ocultar a senha

--- src/Pages/passwords/componets/PasswordEditor.js
+++ src/Pages/passwords/componets/PasswordEditor.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import { Pressable, View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons"
 
 
 export default function PasswordEditor({data, removePassword}) {
+
+    const[senhaVizivel, setSenhaVizivel] = useState(true)
+    const[animacaoOlho, setAnimacaoOlho] = useState("eye")
+
+    function vizibilidade(){
+        if(senhaVizivel){
+            setAnimacaoOlho("eye-off-outline")
+            setSenhaVizivel(false)
+        } else {
+            setAnimacaoOlho("eye")
+            setSenhaVizivel(true)
+        }   
+    }
+
     return (
         <SafeAreaView>
             <View>
-                <Pressable onLongPress={removePassword} style={styles.container}>
-                    <Text style={{color: "white"}}>{data}</Text>
+                <Pressable 
+                    onLongPress={removePassword} style={styles.container}> 
+                    
+                    <View>
+                        {senhaVizivel ? (<Text style={{color: "white"}}>{data}</Text>): 
+                        <Text style={styles.barraBranca}></Text>}
+                    </View>
+                    <Pressable onPress={vizibilidade}>
+                        <Ionicons name={animacaoOlho} color={"white"} size={20}/>
+                    </Pressable>
+                    
                 </Pressable>
             </View>
         </SafeAreaView>
@@ -22,6 +46,12 @@ const styles = StyleSheet.create({
         padding: 14, 
         width: "100%",
         borderRadius: 8,
-        justifyContent: "center"
+        justifyContent: "space-between", 
+        flexDirection: "row",
+    },
+    barraBranca: {
+        backgroundColor: "white",
+        width: 200,
+        borderRadius: 4, 
     }
 })
